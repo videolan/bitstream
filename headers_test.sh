3488ec8596e62aec48314c2eab3ba842d486bfe9
Georgi Chorbadzhiyski <gf@unixsol.org>
2011-10-03 19:34:52 +0300

    Add script that checks if every biTStream header can be used standalone.
    Add headers_test.sh. A script that checks if every biTStream header
    can be used standalone (includes all needed headers to implement
    its functionality).

---
+++ headers_test.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# Test if each header can be used standalone
+# Copyright (C) 2011 Unix Solutions Ltd.
+#
+# License: MIT
+# Authors: Georgi Chorbadzhiyski <georgi@unixsol.org>
+#
+
+HEADER_LIST="dvb/sim.h ietf/rtp.h"
+# Find is used in such a way in order to work on OS X as well as Linux
+# If you want to test only particular headers comment the line bellow
+HEADER_LIST=$(find * | grep \\.h$ | sort | sed -e 's|^\./||')
+
+for HDR in $HEADER_LIST
+do
+    test_file=$(echo $HDR | sed -e 's|/|_|g;s|\.h$||')
+    echo "Testing: $HDR"
+    printf "#include \"$HDR\"\n\nint main(void) { return 0; }\n" > $test_file.c
+    gcc -I.. -Werror -Wall -Wextra -Wno-unused -Wno-sign-compare -Wformat-security $test_file.c -o $test_file
+    [ $? != 0 ] && exit 1
+    rm $test_file $test_file.c
+done
