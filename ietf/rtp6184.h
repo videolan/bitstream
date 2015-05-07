dd6c28a7e7866de60150509c013f2ac6075c8b36
Arnaud de Turckheim <quarium@gmail.com>
2015-05-07 17:04:46 +0200

    add ietf/rtp6184.h

---
+++ ietf/rtp6184.h
@@ -0,0 +1,9 @@
+/*
+ * RTP Payload Format for H.264 Video
+ */
+#ifndef __BITSTREAM_IETF_RTP6184_H__
+# define __BITSTREAM_IETF_RTP6184_H__
+
+#define RTP_6184_CLOCKRATE       90000
+
+#endif /* !__BITSTREAM_IETF_RTP6184_H__ */
