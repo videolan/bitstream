87474f57f90f8b312f1cce96152f7d24aa7f3321
Kieran Kunhya <kierank@ob-encoder.com>
2015-08-19 22:56:34 +0100

    Add Opus RTP payload format header

---
+++ ietf/rtp7587.h
@@ -0,0 +1,9 @@
+/*
+ * RTP Payload Format for the Opus Speech and Audio Codec
+ */
+#ifndef __BITSTREAM_IETF_RTP7587_H__
+# define __BITSTREAM_IETF_RTP7587_H__
+
+#define RTP_7587_CLOCKRATE       48000
+
+#endif /* !__BITSTREAM_IETF_RTP7587_H__ */
