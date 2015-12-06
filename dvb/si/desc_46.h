70516b167534691d73ff99e3b61cc5f61135013b
Christophe Massiot <cmassiot@openheadend.tv>
2015-12-06 02:21:36 +0100

    fix bad printing of vbi & telx descriptors

--- dvb/si/desc_46.h
+++ dvb/si/desc_46.h
@@ -131,7 +131,7 @@ static inline void desc46_print(uint8_t *p_desc, f_print pf_print,
         switch (i_print_type) {
         case PRINT_XML:
             pf_print(opaque,
-                 "<%s language=\"%3.3s\" type=\"0x%hhx\" type_txt=\"%s\" mag=\"%hhu\" page=\"0x%hhux\"/>",
+                 "<%s language=\"%3.3s\" type=\"0x%hhx\" type_txt=\"%s\" mag=\"%hhu\" page=\"0x%hhx\"/>",
                  desc_get_tag(p_desc) == 0x46 ? "VBI_TELX_DESC" : "TELX_DESC",
                  (const char *)desc46n_get_code(p_desc_n),
                  desc46n_get_teletexttype(p_desc_n),
@@ -141,7 +141,7 @@ static inline void desc46_print(uint8_t *p_desc, f_print pf_print,
             break;
         default:
             pf_print(opaque,
-                 "    - desc %x %s language=%3.3s type=0x%hhx type_txt=\"%s\" mag=%hhu page=0x%hhux",
+                 "    - desc %x %s language=%3.3s type=0x%hhx type_txt=\"%s\" mag=%hhu page=0x%hhx",
                  desc_get_tag(p_desc),
                  desc_get_tag(p_desc) == 0x46 ? "vbi_telx" : "telx",
                  (const char *)desc46n_get_code(p_desc_n),
