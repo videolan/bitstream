30b7a8ad8584ae4a523703068c6bba04835caa29
nriviera <ragazzo.ionico@gmail.com>
2019-10-28 10:51:47 +0100

    descriptors 0x38 and 0x3f support

--- mpeg/psi/descs_list.h
+++ mpeg/psi/descs_list.h
@@ -68,5 +68,7 @@
 #include <bitstream/mpeg/psi/desc_2a.h>
 #include <bitstream/mpeg/psi/desc_2b.h>
 #include <bitstream/mpeg/psi/desc_2c.h>
+#include <bitstream/mpeg/psi/desc_38.h>
+#include <bitstream/mpeg/psi/desc_3f.h>
 
 #endif
